8e2dadd17a68d6f14c6090da8f9486a6d7e61656
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-10-31 09:33:48 +0000

    Fix regex to stop certain links slipping through

--- url-defense-stripper.js
+++ url-defense-stripper.js
@@ -1,31 +1,36 @@
 const rewrite = async () => {
+    const urlDefenseMatch = "https://urldefense.com/v./__([^\\s]+)__;.+";
+    const hrefMatch = new RegExp(urlDefenseMatch);
+    const textMatch = new RegExp(urlDefenseMatch + "\\$(</a>)?( \\[.+\\])?", "g");
+
     // Replace HTML links
-    const links = document.getElementsByTagName('a');
+    const links = document.getElementsByTagName("a");
     for (let link of links) {
-        if (link.href.startsWith('https://urldefense.com')) {
+        if (link.href.startsWith("https://urldefense.com")) {
             // Re-write href
-            const matches = link.href.match(/https:\/\/urldefense.com\/v.\/__(.+)__;.+/);
+            const matches = link.href.match(hrefMatch);
             if (matches && matches.length) {
-                link.href = matches[1].replaceAll('*', '#'); // # gets replaced by *, reverse that (may catch legitimate *?)
+                link.href = matches[1].replaceAll("*", "#"); // # gets replaced by *, reverse that (may catch legitimate *?)
             }
 
-            // Re-write text ("blabla [some.domain.com]")
-            const textMatches = link.innerText.match(/(.+) \[.+\]/);
-            if (textMatches && textMatches.length) {
-                link.innerText = textMatches[1];
+            // Re-write link's inner contents to remove square bracket bit ("blabla [some.domain.com]")
+            const innerMatches = link.innerHTML.match(/(.*) \[.+\]/);
+            if (innerMatches && innerMatches.length) {
+                link.innerHTML = innerMatches[1];
             }
         }
     }
 
     // Replace plaintext links
     let text = document.body.innerHTML;
-    const iter = text.matchAll(/https:\/\/urldefense.com\/v.\/__(.+)__;.+\$(<\/a>)?( \[.+\])?/g);
+    const iter = text.matchAll(textMatch);
     let value;
     let modified = false;
     while (value = iter.next().value) {
-        let replacement = value[1].replaceAll('*', '#');
-        if (value[2]) // Replacing closing </a> if present
+        let replacement = value[1].replaceAll("*", "#");
+        if (value[2]) { // Replacing closing </a> if present
             replacement = replacement + value[2];
+        }
         text = text.replaceAll(value[0], replacement);
         modified = true;
     }
