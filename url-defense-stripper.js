4682bd58b684b27b6134e637f433e5c59e17faf3
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-11-05 09:52:39 +0000

    Fix lossy replacement

--- url-defense-stripper.js
+++ url-defense-stripper.js
@@ -1,7 +1,38 @@
+const util = {
+    _key: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+
+    decode: function (str) {
+        return atob(str.replace(/_/g, '/').replace(/-/g, '+'));
+    },
+
+    length: function (char) {
+        const l = this._key.indexOf(char);
+        return (l === -1) ? null : l + 2;
+    },
+
+    replaceSubs: function (url, key) {
+        let subs = this.decode(key).split("");
+        const iter = url.matchAll(/\*\*?([-_A-Za-z0-9])?/g);
+        let drift = 0;
+        let value
+        while (value = iter.next().value) {
+            const startIndex = value.index + drift;
+            const endIndex = value.index + value[0].length + drift;
+            const length = value[1] ? util.length(value[1]) : 1;
+            const replacement = subs.splice(0, length).join("");
+
+            url = url.slice(0, startIndex) + replacement + url.slice(endIndex, url.length);
+            drift += (replacement.length - value[0].length);
+        }
+
+        return url;
+    }
+}
+
 const rewrite = async () => {
-    const urlDefenseMatch = "https://urldefense.com/v./__([^\\s]+)__;.+";
+    const urlDefenseMatch = "https://urldefense.com/v./__([^\\s]+)__;([^!]*)!!.+";
     const hrefMatch = new RegExp(urlDefenseMatch);
-    const textMatch = new RegExp(urlDefenseMatch + "\\$(</a>)?( \\[.+\\])?", "g");
+    const textMatch = new RegExp(urlDefenseMatch + "\\$(</a>)?( ?\\[.+\\])?", "g");
 
     // Replace HTML links
     const links = document.getElementsByTagName("a");
@@ -10,7 +41,7 @@ const rewrite = async () => {
             // Re-write href
             const matches = link.href.match(hrefMatch);
             if (matches && matches.length) {
-                link.href = matches[1].replaceAll("*", "#"); // # gets replaced by *, reverse that (may catch legitimate *?)
+                link.href = util.replaceSubs(matches[1], matches[2]);
             }
 
             // Re-write link's inner contents to remove square bracket bit ("blabla [some.domain.com]")
@@ -27,9 +58,9 @@ const rewrite = async () => {
     let value;
     let modified = false;
     while (value = iter.next().value) {
-        let replacement = value[1].replaceAll("*", "#");
-        if (value[2]) { // Replacing closing </a> if present
-            replacement = replacement + value[2];
+        let replacement = util.replaceSubs(value[1], value[2]);
+        if (value[3]) { // Replacing closing </a> if present
+            replacement = replacement + value[3];
         }
         text = text.replaceAll(value[0], replacement);
         modified = true;
